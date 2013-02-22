80ba09e06c597965c5ca1b49622fa20a1ccf63fd
Evan Stuart <evan@macarthur.(none)>
2013-02-22 16:36:06 -0600

    most recent

--- m_carve.php
+++ m_carve.php
@@ -1,8 +1,8 @@
 <?php
 
-//performs carve accross multiple files
+//carves out files to search through
 function carve($start, $stop)
-{    
+{   $carve_results; 
 	//if true: files are different
 	if(strcmp($start,$stop)){
 		print "Start File : ".$start."\n";
@@ -18,21 +18,24 @@ function carve($start, $stop)
 			//assign start and end timestamp to variables
 			$start_tstamp = $start_token[1];
 			$stop_tstamp  = $stop_token[1];
-			
+			print "Start Tstamp : ".$start_tstamp;
+			print "\nStop Tstamp  : ".$stop_tstamp."\n";
 			//store capture directory contents into variable
 			//have to extract since it was passed from list_dir function
 			$dircontents = list_dir("../test_data/");
 			extract($dircontents);
-			
+			print "# of files in dir: ".count($dircontents)."\n";
 			//if first x digits match (in this case 6) then treat them as matches
 			for($i=0; $i<(count($dircontents)); $i++){
-				$temp = $dircontents[$i];										//may not need to do this since i fixed the if statemtn below
-				print "temp: ".$temp;
-				if(substr_compare($start_tstamp,$temp,0,5)==0){					//currently not working correctly
-					print "\nmatch: ".$start_tstamp."\n".$dircontents[$i]."\n\n";
+				$temp = $dircontents[$i];	
+				print substr($start_tstamp,0,6)."\n";								//may not need to do this since i fixed substr issue
+				if(substr_compare($start_tstamp,$temp,0,5)==0){							//could replace 5 with user input on how many digits to check
+					//print "\nmatch: ".$start_tstamp."\n".$dircontents[$i]."\n\n";
+					$carve_results[$i]=$dircontents[$i];
+					print "Match :".$dircontents[$i]."\n"; 
 				}
-				 else{
-				 	print "other";
+				else{
+				 	print "No Match: ".$dircontents[$i]."\n";
 				 }
 			}
 		}
@@ -41,21 +44,30 @@ function carve($start, $stop)
 	else{
 		print "Files are the same, use cxt2pcap.pl dumbass!\n";
 	}
+	//extract($carve_results);
+	print "\n1 before sort :".$carve_results[5];
+	asort($carve_results);
+	print "\n1 After  sort :".$carve_results[3];
+	//var_dump($carve_results);
+	return $carve_results;
 }
 
 function list_dir($directory)
 {
 	$open_directory = opendir($directory);
-		while($filename = readdir($open_directory)){
-			$filesplit = explode(".", $filename);
-			$check_filename = $filesplit[0];
-				if($check_filename=="cxt"){
-					$valid_files[] = $filesplit[1];
-				}
-
+	while($filename = readdir($open_directory)){
+		$filesplit = explode(".", $filename);
+		$check_filename = $filesplit[0];
+		//add file endings to first row of arrray(i)
+		//add corresponding file sizes to next row(j)
+		if($check_filename=="cxt"){
+			$valid_files[] = $filesplit[1];
 		}
-	closedir($directory);
-	sort($valid_files);
+	}
+
+	//closedir($directory);
+	asort($valid_files);
+	//var_dump($valid_files);
 	return $valid_files;
 
 
@@ -78,6 +90,8 @@ if (count($options)!=2){
 $start = $options["s"];
 $stop  = $options["e"];
 //
-carve($start,$stop);
-
+$file_range[] = carve($start,$stop);
+extract($file_range);
+var_dump($filerange);
+//now call cxt2pcap.pl with user inputted parameters plus filerange and filesizes
 ?>
