4812faa8ae383cb50aea0101c6a669c441aa687f
Evan Stuart <estuart@INNC-ES-L01.(none)>
2013-02-22 21:06:18 -0500

    added todo file, and cleaned up some sloppy comments. Also added filesize function (not implemented)

--- m_carve.php
+++ m_carve.php
@@ -1,8 +1,8 @@
 <?php
 
-//carves out files to search through
+//carves out files that fit given start and stop range
 function carve($start, $stop)
-{   $carve_results; 
+{ 
 	//if true: files are different
 	if(strcmp($start,$stop)){
 		print "Start File : ".$start."\n";
@@ -10,7 +10,7 @@ function carve($start, $stop)
 		//tokenize start and end file name by (.)
 		$start_token = explode(".",$start);
 		$stop_token  = explode(".",$stop);
-		//check to make sure file is in partA.partB format (size 2)
+		//check to make sure file is in (prefix).(suffix) format (size 2)
 		if(count($start_token)!=2 or count($stop_token)!=2){
 			print "Invalid file name format, must be two strings separated by a period (.)\n";
 		}
@@ -22,33 +22,33 @@ function carve($start, $stop)
 			print "\nStop Tstamp  : ".$stop_tstamp."\n";
 			//store capture directory contents into variable
 			//have to extract since it was passed from list_dir function
-			$dircontents = list_dir("../test_data/");
+			$dircontents = list_dir("../scripts/data");
 			extract($dircontents);
-			print "# of files in dir: ".count($dircontents)."\n";
 			//if first x digits match (in this case 6) then treat them as matches
-			for($i=0; $i<(count($dircontents)); $i++){
-				$temp = $dircontents[$i];	
-				print substr($start_tstamp,0,6)."\n";								//may not need to do this since i fixed substr issue
-				if(substr_compare($start_tstamp,$temp,0,5)==0){							//could replace 5 with user input on how many digits to check
-					//print "\nmatch: ".$start_tstamp."\n".$dircontents[$i]."\n\n";
-					$carve_results[$i]=$dircontents[$i];
-					print "Match :".$dircontents[$i]."\n"; 
+			//and add to the results array
+			$j=$i=0;
+			for($i; $i<(count($dircontents)); $i++){
+				
+				if(substr_compare($start_tstamp,$dircontents[$i],0,5)==0){		
+					$carve_results[$j]=$dircontents[$i];
+					$j++;
 				}
 				else{
-				 	print "No Match: ".$dircontents[$i]."\n";
-				 }
+				 	 ;//do nothing
+				}
 			}
 		}
 	}
-	//else: Files are the same
 	else{
 		print "Files are the same, use cxt2pcap.pl dumbass!\n";
 	}
-	//extract($carve_results);
-	print "\n1 before sort :".$carve_results[5];
-	asort($carve_results);
-	print "\n1 After  sort :".$carve_results[3];
-	//var_dump($carve_results);
+	
+	//sort results and return array
+	sort($carve_results);
+	print "Searched ".count($dircontents)." files and found ".count($carve_results)." matching criteria:\n";
+	for($i=0;$i<count($carve_results);$i++){
+		print "cxt.".$carve_results[$i]."\n";
+	}
 	return $carve_results;
 }
 
@@ -56,23 +56,32 @@ function list_dir($directory)
 {
 	$open_directory = opendir($directory);
 	while($filename = readdir($open_directory)){
+		print filesize($directory."/".$filename)."\n";
+		
 		$filesplit = explode(".", $filename);
 		$check_filename = $filesplit[0];
 		//add file endings to first row of arrray(i)
 		//add corresponding file sizes to next row(j)
-		if($check_filename=="cxt"){
+		#can add user defined prefix later
+		if($check_filename=="cxt"){						
 			$valid_files[] = $filesplit[1];
 		}
 	}
-
-	//closedir($directory);
-	asort($valid_files);
-	//var_dump($valid_files);
+	//need to closedir()
+	//return valid files to be searched through
 	return $valid_files;
+}
 
+function get_sizes($files_array){
+	//calculate file sizes of carved files
 
 
 }
+
+function cxt2pcap(){
+	print "placeholder";
+}
+
 
 //define command line arguments
 $shortopts  = "";
@@ -90,8 +99,11 @@ if (count($options)!=2){
 $start = $options["s"];
 $stop  = $options["e"];
 //
-$file_range[] = carve($start,$stop);
+$file_range = carve($start,$stop);
+//$file_sizes = get_sizes($file_range)
 extract($file_range);
-var_dump($filerange);
+var_dump($file_range);
 //now call cxt2pcap.pl with user inputted parameters plus filerange and filesizes
+
 ?>
+
